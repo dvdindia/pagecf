012cdb722087374fa7a29c8ed2e028fcea51b60d
dvdindia <106233149+dvdindia@users.noreply.github.com>
2022-05-26 10:29:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,14 +1,14 @@
 export default {
   async fetch(request, env) {
-    const day1 = 'sanditable.herokuapp.com'
-    const day2 = 'threetables.herokuapp.com'
+    const day1 = 'sanditable.herokuapp.com';
+    const day2 = 'threetables.herokuapp.com';
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      let day = new Date()
+      let day = new Date();
       if (day.getDay() % 2) {
-        url.hostname = day1
+        url.hostname = day1;
       } else {
-        url.hostname = day2
+        url.hostname = day2;
       }
       let new_request = new Request(url, request);
       return fetch(new_request);
